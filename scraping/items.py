59749c0be526b84d6080f2dbfd5f5c60b3da5b95
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-05-02 17:54:32 +0200

    Create VacancyItem, with serializers to each field and Create class VacancyTools for getting tools out of description

--- scraping/items.py
+++ scraping/items.py
@@ -2,11 +2,94 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+import re
+
+import nltk
+from nltk.corpus import stopwords
 
 import scrapy
+from scrapy import Field
+from w3lib.html import remove_tags
+
+from config import POSITION
 
 
-class ScrapingItem(scrapy.Item):
+nltk.download("stopwords")
+
+
+class VacancyTools:
+    """
+    Class for getting toolkits from text, it's just looking
+    for capitalizing words in the text, which is a common way.
+    Plus going through filters like: removing stopwords/duplicates.
+    I am not promising that it will return only toolkits.
+    """
+    def __init__(self, text: str) -> None:
+        self.text = text
+
+    def get_tools(self) -> set:
+        """
+        There are a lot of redundant info in description like
+        information about the project/company etc. We need to aim where
+        skills/requirements are specified.
+        """
+        texts = [
+            text.replace("<br>", ". ")
+            for text in self.text.split("<br><br>")
+            if POSITION.lower() in text.lower()
+        ]
+        print(texts)
+        return set().union(*(self.filter_text(text) for text in texts))
+
+    @staticmethod
+    def filter_text(text: str) -> set[str]:
+        """Aims for getting tools from text, by finding capitalize words"""
+        # we do not want to match this words
+        ignore_match = (
+            r"(?<!^)"  # ignore words at the beginning of a new line
+            r"(?<![^\x00-\x7F])"  # '•Design'
+            r"(?<![-.*+)>:])(?<![-.*+)>:] )"  # - Competitive | .Competitive
+            r"(?<![^\x00-\x7F] )\b"  # '• Design' with space
+        )
+        # matching pattern
+        match = (
+            r"(?:[A-Z][a-z]*)+"  # Power
+            r"(?:\W(?:[A-Z][a-z]*)+)*"  # Power BI or Power-BI
+            # ignore words with ":" at the end ex. 'Skills:'
+            r"\b(?!(?: (?:[A-Z][a-z]*)+)*:)"
+        )
+        matches = set(re.findall(ignore_match + match, text))
+        return matches
+
+    @staticmethod
+    def removing_stopwords(words: set[str]) -> set[str]:
+        """Removing stopwords, using NLTK stopwords"""
+        return {word for word in words if word.lower() not in set(stopwords.words())}
+
+    @staticmethod
+    def removing_duplicates(words: set[str]) -> set[str]:
+        """Removing duplicates, exp 'AI', 'AI services' - is same, 'AI' should stay"""
+        unq_words = list()
+        for word in sorted(list(words)):
+            if not (unq_words and word.startswith(unq_words[-1])):
+                unq_words.append(word)
+        return set(unq_words)
+
+    def get_clean_tools(self) -> set[str]:
+        """Applying all filters to get tools for POSITION"""
+        return self.removing_duplicates(self.removing_stopwords(self.get_tools()))
+
+
+def first_integer(value: str) -> int:
+    return int(value.split()[0])
+
+
+class VacancyItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    date = Field(serializer=lambda v: v.split()[1])
+    num_views = Field(serializer=first_integer)
+    num_applications = Field(serializer=first_integer)
+    tools = Field(serializer=lambda v: VacancyTools(text=v).get_clean_tools())
+    year_of_exp = Field(serializer=first_integer)
+    employment_type = Field(serializer=lambda v: v.split(" or "))
+    country = Field(serializer=lambda v: v.strip().replace("\n   ", "").split(", "))
