2963e5253b18d8ac94a8f3e6c6613c972c78ae8d
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-05-02 17:56:01 +0200

    Create VacancyScraper for scraping vacancies from djinni

---
+++ scraping/spiders/vc_scraper.py
@@ -0,0 +1,42 @@
+"""
+Vacancy scraper module
+"""
+from typing import Any
+
+import scrapy
+from scrapy.http import Response
+
+import config
+from scraping.items import VacancyItem
+
+
+class VacancyScraper(scrapy.Spider):
+    name = "vacancies"
+    start_urls = [config.JOBS_URL]
+    page = 1
+
+    def parse(self, response: Response, **kwargs: Any) -> VacancyItem:
+        # get vacancies
+        for vc in response.xpath("//li[contains(@id, 'job-item-')]"):
+            yield self.parse_vc(vc, **kwargs)
+
+    # next page
+        yield from response.follow_all(
+            css=".pagination li:nth-last-child(1) a",
+            callback=self.parse
+        )
+
+    def parse_vc(self, vc: Response, **kwargs) -> VacancyItem:
+        return VacancyItem(**{
+            "date": vc.xpath(".//span[@class='mr-2 nobr']/@title").get(),
+            "num_views": vc.xpath(".//span[@class='nobr']/span[1]/@title").get(),
+            "num_applications": vc.xpath(".//span[@class='nobr']/span[2]/@title").get(),
+            "tools": vc.xpath(".//span/@data-original-text").get(),
+            "year_of_exp": vc.xpath(
+                ".//span[contains(text(), ' of experience')]/text()"
+            ).get(default=0),
+            "employment_type": vc.xpath(
+                ".//span[contains(text(), 'Remote')"
+                " or contains(text(), 'Office')]/text()").get(),
+            "country": vc.xpath(".//span[@class='location-text']/text()").get()
+        })
