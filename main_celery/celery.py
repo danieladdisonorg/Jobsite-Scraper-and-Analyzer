6ff0ec1d27f0254aad48f1841645789fbb4c10a5
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-12-17 19:58:41 +0100

    Create main celery directory, set up schedule, create 2 Queues

---
+++ main_celery/celery.py
@@ -0,0 +1,48 @@
+import os
+import dotenv
+from celery import Celery
+
+dotenv.load_dotenv()
+
+
+celery_app = Celery(
+    "main_celery",
+    broker=os.getenv("CELERY_BROKER_URL"),
+    backend=os.getenv("CELERY_RESULT_BACKEND")
+)
+
+# Route 'scraping' and 'web_server' tasks to queues
+celery_app.conf.task_routes = {
+    "scraping.tasks.*": {"queue": "scraping_queue"},
+    "web_server.tasks.*": {"queue": "web_server_queue"},
+}
+
+# Define Queues
+celery_app.conf.task_queues = {
+    "scraping_queue": {"binding_key": "scraping_queue"},
+    "web_server_queue": {"binding_key": "web_server_queue"},
+}
+
+# Allow tasks to register schedules
+celery_app.conf.update(
+    timezone="UTC",
+    beat_schedule={},
+)
+
+
+# Load schedules from registered modules
+def load_schedules():
+    """
+    Registering tasks schedule
+    """
+    from scraping.tasks import start_scraping_schedule
+
+    celery_app.conf.beat_schedule.update(
+        start_scraping_schedule
+    )
+
+
+# register tasks
+celery_app.autodiscover_tasks()
+
+load_schedules()
