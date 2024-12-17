555ba05888d76af67345f3c97d3f1dcda59e99ab
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-12-17 20:02:24 +0100

    Create scraping/tasks.py to let celery process diagrams, for efficiency reason.

--- scraping/tasks.py
+++ scraping/tasks.py
@@ -1,4 +1,7 @@
+import os
 import logging
+from datetime import timedelta
+
 from celery import shared_task
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
@@ -9,7 +12,7 @@ from scraping.spiders.vc_scraper import VacancyScraper
 logging.info("Tasks module loaded!")
 
 
-@shared_task
+@shared_task(queue="scraping_queue")
 def start_scraping():
     logging.info("Scraping task started!")
 
@@ -18,3 +21,19 @@ def start_scraping():
     process.start()
 
     logging.info("Scraping task finished!")
+
+
+# set up scraping_schedule schedule to run every 'SCRAPING_EVERY_NUM_DAY'
+start_scraping_schedule = {
+    "scrapy_every_num_days": {
+        "task": "scraping.task.start_scraping",
+        "schedule": timedelta(
+            days=int(os.getenv("SCRAPING_EVERY_NUM_DAY"))
+        )
+    }
+}
+
+# also when web application is up.
+# We should run 'start_scraping' manually ones
+# and next run we wait every 'SCRAPING_EVERY_NUM_DAY'.
+start_scraping.delay()
