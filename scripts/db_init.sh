de3fbb91479e56cbe12787b7528a9e3ca976e874
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-12-19 21:48:24 +0100

    modify db_init.sh to retry several times to connect db before quiting

--- scripts/db_init.sh
+++ scripts/db_init.sh
@@ -2,12 +2,19 @@
 
 HOST=$1
 PORT=$2
+RETRIES=30
 
 # wait for DataBase connection
-until nc -z -v -w30 $HOST $PORT
-do
+until nc -z -v -w30 $HOST $PORT || [ $RETRIES -eq 0 ]; do
+
   echo "Waiting for DataBase at $HOST:$PORT ...."
+  ((RETRIES--))
+  sleep 1
 done
+if [ $RETRIES -eq 0 ]; then
+  echo "DataBase is not reachable, exiting."
+  exit 1
+fi
 
 echo "DataBase is up and running at $HOST:$PORT"
 
