2a524b0fa7b7f8a7e163978eb427f81423d6cb67
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-12-19 21:52:13 +0100

    Remove last message from entrypoint.sh because it leads to error

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-c
+
 # Wait for database to be ready
-./scripts/wait_for_db.sh db:3306
+scripts/db_init.sh db 3306
 
 echo "Running App using Gunicorn"
-gunicorn -b 0.0.0.0:5000 --timeout 1000 run:app
-echo "App is running successfully!!!"
+gunicorn -b 0.0.0.0:5000 run:app
