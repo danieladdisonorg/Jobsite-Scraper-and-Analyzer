480e02b1fd5928f483f05ce0fcf3ff8ee0154144
danieladdisonorg <wiljansantiago0424@gmail.com>
2024-12-25 14:01:16 +0100

    refact(main_celery/__init__.py): Better discover tasks

--- main_celery/__init__.py
+++ main_celery/__init__.py
@@ -1,6 +1,8 @@
-from scraping import tasks
-from web_server import tasks
+from scraping import tasks as scr_tasks
+from web_server import tasks as web_tasks
 
 """
 Allow Celery to discover tasks
 """
+
+__all__ = ["scr_tasks", "web_tasks"]
